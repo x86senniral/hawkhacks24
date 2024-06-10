15613240c3cd5b755efd848ca252d2eefac8c4cd
x86senniral <60050784+x86senniral@users.noreply.github.com>
2024-06-09 21:04:39 -0400

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -16,4 +16,3 @@ export const connectDB = async () => {
   }
 };
 
-//neurelo_9wKFBp874Z5xFw6ZCfvhXcs7m/qx/0Lte4zBlOC3euCBGHpb4oOzZDLpjfxXEQPpdhpsZfD3xLyYOWPARVPx8OZjOhGZjI5MdpkQdfsxE97oDhPzGh4TODzJXUWcbNBxnQW+S6QvK2xHJYBl2YT2OFF2ya62h+8EgyNw7VvGiyRyjtywpie9J7B2JUUVjfzI_7xigvAn5FvoSD/Kje1G0yHWq2YJQwYr6MkKtPYqbLOE=
